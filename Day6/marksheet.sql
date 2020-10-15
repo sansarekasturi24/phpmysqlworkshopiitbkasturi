7428a3b5ad2dadeed826f07562d50215626bb4fe
unknown <sansarekasun19ce@student.mes.ac.in>
2020-10-15 23:02:51 +0530

    ninth commit

--- Day6/marksheet.sql
+++ Day6/marksheet.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 15, 2020 at 06:35 PM
+-- Generation Time: Oct 15, 2020 at 07:30 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.2
 
